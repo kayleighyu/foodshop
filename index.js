83e2a9dddaa83c32e560c98c3fa34679ba2ca45d
Kayleigh Yu <kayleighyu@gmail.com>
2017-03-10 20:21:39 -0500

    Successfully implemented the delete /foods, put /foods/:id, post /foods, get /foods/price/:dollarAmount, and delete /foods/category/:category routes.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var server = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 8080;
 
 var mongoURI = process.env.MONGOURI || require('./secrets').mongoURI;
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect(mongoURI);
 
@@ -69,6 +73,77 @@ server.get('/foods/category/:categoryName', function(req, res){
     } else {
       res.status(200).json({
         foods: documents
+      });
+    }
+  });
+});
+
+server.get('/foods/price/:dollarAmount', function(req, res){
+  Food.find({price: req.params.dollarAmount}, function(err, documents){
+    if (err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        foods: documents
+      });
+    }
+  });
+});
+
+server.post('/foods', function(req, res){
+  var food = new Food(req.body);
+  food.save(function(err, documents){
+    if (err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(201).json({
+        msg: 'New food successfully created'
+      });
+    }
+  });
+});
+
+server.put('/foods/:id', function(req, res){
+  Food.findOneAndUpdate({_id: req.params.id}, req.body, function(err, documents){
+    if (err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        msg: 'Food succesfully updated!'
+      });
+    }
+  });
+});
+
+server.delete('/foods/:id', function(req, res){
+  Food.remove({_id: req.params.id}, function(err, documents){
+    if (err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        msg: 'Food successfully deleted!'
+      });
+    }
+  });
+});
+
+server.delete('/foods/category/:category', function(req, res){
+  Food.remove({category: req.params.category}, function(err, documents){
+    if (err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        msg: 'Successfully deleted all foods in the category ' + req.params.category + '!'
       });
     }
   });
